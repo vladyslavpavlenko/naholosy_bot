27c1a4c291d7f98e66252403f5ab5aac0469dc2e
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2025-06-04 02:12:47 +0300

    feat: add field abstraction to logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -16,11 +16,6 @@ type Logger struct {
 	z *zap.Logger
 }
 
-// Get exposes the underlying *zap.Logger
-func (l *Logger) Get() *zap.Logger {
-	return l.z
-}
-
 func New(level string) *Logger {
 	var cfg zap.Config
 	switch level {
@@ -34,30 +29,62 @@ func New(level string) *Logger {
 	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 
 	sink := zapcore.AddSync(os.Stdout)
-
 	encoder := zapcore.NewConsoleEncoder(cfg.EncoderConfig)
-
 	core := zapcore.NewCore(encoder, sink, cfg.Level)
 
 	return &Logger{z: zap.New(core)}
 }
 
-func (l *Logger) With(fields ...zap.Field) *Logger {
-	return &Logger{z: l.z.With(fields...)}
+// A Field is a marshaling operation used to add a key-value pair
+// to a logger's context.
+type Field struct {
+	zField zap.Field
 }
 
-func (l *Logger) Debug(msg string, fields ...zap.Field) {
-	l.z.Debug(msg, fields...)
+// Param takes a key and an arbitrary value and chooses the best way to
+// represent them as a field.
+func Param(key string, val any) Field {
+	return Field{zField: zap.Any(key, val)}
 }
 
-func (l *Logger) Info(msg string, fields ...zap.Field) {
-	l.z.Info(msg, fields...)
+// Error is shorthand for the common idiom NamedError("error", err).
+func Error(err error) Field {
+	return Field{zField: zap.Error(err)}
 }
 
-func (l *Logger) Warn(msg string, fields ...zap.Field) {
-	l.z.Warn(msg, fields...)
+// With returns a new Logger with extra fields.
+func (l *Logger) With(fields ...Field) *Logger {
+	return &Logger{z: l.z.With(l.toZapFields(fields)...)}
 }
 
-func (l *Logger) Error(msg string, fields ...zap.Field) {
-	l.z.Error(msg, fields...)
+func (l *Logger) Debug(msg string, fields ...Field) {
+	l.z.Debug(msg, l.toZapFields(fields)...)
+}
+
+func (l *Logger) Info(msg string, fields ...Field) {
+	l.z.Info(msg, l.toZapFields(fields)...)
+}
+
+func (l *Logger) Warn(msg string, fields ...Field) {
+	l.z.Warn(msg, l.toZapFields(fields)...)
+}
+
+func (l *Logger) Error(msg string, fields ...Field) {
+	l.z.Error(msg, l.toZapFields(fields)...)
+}
+
+func (l *Logger) Fatal(msg string, fields ...Field) {
+	l.z.Fatal(msg, l.toZapFields(fields)...)
+}
+
+// toZapFields converts our []Field into []zap.Field.
+func (l *Logger) toZapFields(fields []Field) []zap.Field {
+	if len(fields) == 0 {
+		return nil
+	}
+	zFields := make([]zap.Field, len(fields))
+	for i, f := range fields {
+		zFields[i] = f.zField
+	}
+	return zFields
 }
